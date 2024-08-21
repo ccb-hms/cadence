5ee19c69f0194fc3295605dd054b3aea6529ec60
awerdich <awerdich@gmail.com>
2024-08-21 09:19:42 -0400

    meeting scheduler logic

--- src/cadence/mscheduler.py
+++ src/cadence/mscheduler.py
@@ -34,13 +34,16 @@ def in_list(lst, elements):
     return all(check_lst)
 
 class Meetings:
-    def __init__(self, name_df, meeting_day=2):
+    def __init__(self, name_df, name_list=None, meeting_day=2):
         self.name_df = name_df
+        self.name_list = name_list
         self.meeting_day = meeting_day
         self.col_dict = {'date_col': 'date',
                          'name_col': 'name',
                          'comment_col': 'comment',
         }
+        if name_list is None:
+            self.name_list = list(self.name_df.get(self.col_dict.get('name_col')).unique())
 
     def presenter_list(self, group_col='group', random_state=123):
         presenters_df = self.name_df.sample(frac=1, random_state=random_state)
@@ -53,7 +56,7 @@ class Meetings:
         presenters = merge_alternatively(name_lists[0], name_lists[1])
         return presenters
 
-    def skip_date(self, cal_df, skip_date, skip_comment):
+    def skip_date(self, cal_df, skip_date, skip_comment, skip_name='skipped'):
         date_col = self.col_dict.get('date_col')
         name_col = self.col_dict.get('name_col')
         comment_col = self.col_dict.get('comment_col')
@@ -62,44 +65,40 @@ class Meetings:
         if len(cal_df_skip) > 0:
             cal_df_before = cal_df.loc[cal_df.get(date_col) < skip_date]
             cal_df_after = cal_df.loc[cal_df.get(date_col) > skip_date]
-            name_list = list(cal_df.get(name_col).unique())
             # Re-create the calendar for the dates after the skip date
             cal_df_after = self.create_timeboard(start_date=cal_df_after[date_col].min(),
                                                  end_date=cal_df_after[date_col].max(),
                                                  start_name=cal_df_skip.get('name').values[0])
+            cal_df_skip.loc[cal_df_skip.get(date_col) == skip_date, name_col] = skip_name
             cal_df = pd.concat([cal_df_before, cal_df_skip, cal_df_after], axis=0, ignore_index=True)
         else:
             raise ValueError(f'Skip date {skip_date} is not in data.')
         return cal_df
 
     def swap_dates(self, cal_df, date_1, date_2):
-        cal_df_new = cal_df.copy()
-        # Convert the dates to datetime objects
-        lst_dt = [pd.to_datetime(date_1), pd.to_datetime(date_2)]
-        # Check if dates in the table
-        check_dates = in_list(lst=cal_df_new['date'].values, elements=lst_dt)
+            cal_df_new = cal_df.copy()
+            # Convert the dates to datetime objects
+            lst_dt = [pd.to_datetime(date_1), pd.to_datetime(date_2)]
+            # Check if dates in the table
+            check_dates = in_list(lst=cal_df_new['date'].values, elements=lst_dt)
+            if check_dates:
+                # Make copies of the rows we want to swap and then swap the dates.
+                df0 = cal_df_new.loc[cal_df_new['date'] == lst_dt[0]]
+                df0.loc[df0['date'] == lst_dt[0], 'date'] = lst_dt[1]
+                df1 = cal_df_new.loc[cal_df_new['date'] == lst_dt[1]]
+                df1.loc[df1['date'] == lst_dt[1], 'date'] = lst_dt[0]
+                # Remove rows with the old dates
+                cal_df_new.drop(cal_df_new.loc[cal_df_new['date'].isin(lst_dt)].index, inplace=True)
+                # Add the changed rows back in
+                cal_df_new = pd.concat([df0, df1, cal_df_new], axis=0). \
+                    sort_values(by='date', ascending=True). \
+                    reset_index(drop=True)
+            return cal_df_new
 
-        if check_dates:
-            # Make copies of the rows we want to swap and then swap the dates.
-            df0 = cal_df_new.loc[cal_df_new['date'] == lst_dt[0]]
-            df0.loc[df0['date'] == lst_dt[0], 'date'] = lst_dt[1]
-            df1 = cal_df_new.loc[cal_df_new['date'] == lst_dt[1]]
-            df1.loc[df1['date'] == lst_dt[1], 'date'] = lst_dt[0]
-            # Remove rows with the old dates
-            cal_df_new.drop(cal_df_new.loc[cal_df_new['date'].isin(lst_dt)].index, inplace=True)
-            # Add the changed rows back in
-            cal_df_new = pd.concat([df0, df1, cal_df_new], axis=0). \
-                sort_values(by='date', ascending=True). \
-                reset_index(drop=True)
-        return cal_df_new
-
-    def create_timeboard(self, start_date, end_date, start_name, name_list=None):
+    def create_timeboard(self, start_date, end_date, start_name):
         us_ma_holidays = holidays.country_holidays('US', subdiv='MA')
-        # Create a list of name from the table of names
-        if name_list is None:
-            name_list = list(self.name_df[self.col_dict.get('name_col')].unique())
         # Rotate the speakers so that the start_name comes first
-        nlist = cyclic_permutate(lst_in=name_list, name=start_name)
+        nlist = cyclic_permutate(lst_in=self.name_list, name=start_name)
         # Define the list of speakers
         team_order = tb.RememberingPattern(nlist)
         # Set a weekly marker for every Wednesday
